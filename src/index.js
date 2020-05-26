ca04d4a908ac59dd588577058576fd4e0a48aee0
Dedan Msafari <dedan.msafari@gmail.com>
2020-05-26 13:29:17 +0300

    redux basic tutorial

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("Hello World!");
+import store from "./store";
+import { bugAdded, bugResolved} from "./actionCreators"
+
+store.dispatch(bugAdded('A beginner'))
+store.dispatch(bugResolved(1))
+// store.dispatch({
+//     type: actions,
+//     payload: {
+//       id: 1
+//     }
+// })
+
+console.log(store.getState());
